92d3f6705a8371813d252cd41e114bd14c388a38
admin <romul1988@gmail.com>
2018-11-20 00:15:42 +0300

    add comments

--- case_rukkola.py
+++ case_rukkola.py
@@ -22,37 +22,43 @@ logging.info('DEVICES IN WORK: ' + ', '.join(map(lambda x: x.device_name, device
 def test_find_restoraint(client):
     client.connect()
     MAPS_ME.open_app(client.driver)
+    # мокаем центр москвы
     client.driver.set_location(*MOSCOW_CENTER)
     client.driver.find_element_by_id(MAPS_ME.search).click()
 
+    # выцепляем меню со списком элементов и ищем еду
     frame = client.driver.find_element_by_id(MAPS_ME.recycler)
-
     menu = frame.find_elements_by_class_name('android.widget.TextView')
-
     list(filter(lambda x: x.text == 'Еда', menu))[0].click()
 
+    # элемент не сразу подгружается, нужно подождать. после чего, ищем Рукколу
     wait_elements(client, MAPS_ME.title)
     swipe_until_element_and_click(client, element_id=MAPS_ME.title, element_text=RUKKOLA_NAME)
 
+    # проверяем элементы Рукколы на наличие
     check_simple_sequence(client, [
         MAPS_ME.map_surfaceview, MAPS_ME.av__direction, MAPS_ME.pp__preview, MAPS_ME.tv__title,
         MAPS_ME.tv__subtitle, MAPS_ME.tv__address, MAPS_ME.tv__straight_distance
     ])
 
+    # разворачиваем превью
     preview = client.driver.find_element_by_id(MAPS_ME.pp__preview)
-    wait_element(client, MAPS_ME.pp__preview,
-                 active_element=preview, condition=lambda: preview.location['y'] != 960)
-
+    wait_element(
+        client, MAPS_ME.pp__preview, active_element=preview, condition=lambda: preview.location['y'] != 960
+                 )
     client.driver.swipe(500, 1200, 500, 0)
     client.driver.swipe(500, 1200, 500, 0)
 
+    # проверяем элементы превью
     check_simple_sequence(client, [
         MAPS_ME.today_opening_hours, MAPS_ME.tv__place_website, MAPS_ME.tv__place_cuisine
     ])
 
+    # проверяем телефон и координаты на соответствие
     check_additional_property(client, MAPS_ME.tv__place_phone, property_list=['text'])
     check_additional_property(client, MAPS_ME.tv__place_latlon, property_list={'text': '55.756881, 37.622613'})
 
+    # свапаем координаты на другой вид и проверяем на соответствие
     koord = client.driver.find_element_by_id(MAPS_ME.tv__place_latlon)
     wait_element(client, MAPS_ME.tv__place_latlon, active_element=koord, condition=lambda: 'N' in koord.text)
     check_additional_property(client, MAPS_ME.tv__place_latlon, property_list={'text': '55°45′24.77″N, 37°37′21.41″E'})
